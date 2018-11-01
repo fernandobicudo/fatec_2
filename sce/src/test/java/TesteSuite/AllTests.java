7ad699fe16773acea9999f00bafd2c5cc414894b
Fernando <Fernando@DESKTOP-N7BH8N3>
2018-11-01 20:46:55 -0300

    Inlcuidas classes ServicoEmprestimo e Emprestimo, bem como UC09RegistrarEmprestimo, incluido na suite de testes.

--- sce/src/test/java/TesteSuite/AllTests.java
+++ sce/src/test/java/TesteSuite/AllTests.java
@@ -8,12 +8,16 @@ import com.fatec.sce.SceApplication;
 import com.fatec.sce.SceApplicationTests;
 import com.fatec.sce.TestaConexaoComDB;
 import com.fatec.sce.UC01CadastrarLivro;
+import com.fatec.sce.UC02ConsultarLivro;
 import com.fatec.sce.UC05CadastrarUsuario;
+import com.fatec.sce.UC09RegistrarEmprestimoDeLivro;
 
 @RunWith(Suite.class)
 @SuiteClasses({ 
 	UC01CadastrarLivro.class, 
+	UC02ConsultarLivro.class,
 	UC05CadastrarUsuario.class, 
+	UC09RegistrarEmprestimoDeLivro.class,
 	TestaConexaoComDB.class, 
 	})
 public class AllTests {
---
+++ sce/src/test/java/com/fatec/sce/UC09RegistrarEmprestimoDeLivro.java
@@ -0,0 +1,58 @@
+package com.fatec.sce;
+
+import static org.junit.Assert.*;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.junit.Test;
+import com.fatec.sce.model.Emprestimo;
+import com.fatec.sce.model.Livro;
+import com.fatec.sce.model.Usuario;
+import com.fatec.sce.servico.ServicoEmprestimo;
+
+public class UC09RegistrarEmprestimoDeLivro {
+	static private Livro livro;
+	static private Usuario usuario;
+	static private ServicoEmprestimo servico;
+
+	@Test
+	public void CT01RegistraEmprestimoDeLivro_com_sucesso() {
+		// cenario
+		livro = new Livro();
+		livro.setIsbn("121212");
+		livro.setTitulo("Engenharia de Software");
+		livro.setAutor("Pressman");
+		usuario = new Usuario();
+		usuario.setRa("1111");
+		usuario.setNome("Jose da Silva");
+		servico = new ServicoEmprestimo();
+		// acao
+		Emprestimo resultadoEsperado = servico.empresta(livro, usuario);
+		// verificação
+		assertNotNull(resultadoEsperado);
+	}
+
+	@Test
+	public void CT02RegistraEmprestimoDeLivro_com_sucesso() {
+		// cenario
+		Emprestimo emprestimo = null;
+		// acao
+		emprestimo = ObtemEmprestimo.comDadosValidos();
+		// verificação
+		assertNotNull(emprestimo);
+	}
+
+	@Test
+	public void CT02UC01FB_registra_emprestimo_com_sucesso_validacao_da_data() {
+		// cenario
+		DateTimeFormatter fmt = DateTimeFormat.forPattern("YYYY/MM/dd");
+		String dataEsperada = new DateTime().plusDays(8).toString(fmt);
+		ServicoEmprestimo servico = null;
+		// acao
+		Emprestimo emprestimo = ObtemEmprestimo.comDadosValidos();
+		String dataObtida = emprestimo.getDataDevolucao();
+		// verificacao
+		assertTrue(dataEsperada.equals(dataObtida));
+	}
+}
