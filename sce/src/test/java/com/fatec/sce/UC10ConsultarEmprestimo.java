19b38c8df016cb0061574f4030591cff91b7326a
Fernando <Fernando@DESKTOP-N7BH8N3>
2018-11-05 03:53:49 -0200

    Tentativa de entregar o lab12

--- sce/src/test/java/com/fatec/sce/UC10ConsultarEmprestimo.java
+++ sce/src/test/java/com/fatec/sce/UC10ConsultarEmprestimo.java
@@ -13,15 +13,15 @@ public class UC10ConsultarEmprestimo {
 	@Test
 	public void CT01CadastrarEmprestimo_com_sucesso() {
 //cenario
-		Emprestimo umUsuario = ObtemEmprestimo.comDadosValidos();
+		Emprestimo umEmprestimo = ObtemEmprestimo.comDadosValidos();
 		Emprestimo resultadoObtido = null;
 		DAOFactory mySQLFactory = DAOFactory.getDAOFactory(DAOFactory.MYSQL);
 //		IUsuarioDAO usuarioDAO = mySQLFactory.getUsuarioDAO();
 //acao
-		usuarioDAO.adiciona(umUsuario);
-		resultadoObtido = usuarioDAO.consulta(umUsuario.getRa());
+		emprestimoDAO.adiciona(umEmprestimo);
+		resultadoObtido = emprestimoDAO.consulta(umUsuario.getRa());
 //verificacao
 		assertTrue(resultadoObtido.equals(umUsuario));
-		usuarioDAO.exclui(umUsuario.getRa());
+		emprestimoDAO.exclui(umUsuario.getRa());
 	}
 }
